510cf5c0935088323d00615f2f660ad82846e9c9
sore.gustavo@gmail.com <sore.gustavo@gmail.com>
2017-09-02 16:05:22 -0300

    first commit

---
+++ MyMovie/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyMovie
+//
+//  Created by Gustavo Luís Soré on 02/09/17.
+//  Copyright © 2017 Gustavo Luís Soré. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
