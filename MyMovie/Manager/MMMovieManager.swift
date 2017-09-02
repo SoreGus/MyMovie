2ac171db35a6c447d4253c9e668a5d5c42a09621
Unknown <sore.gustavo@gmail.com>
2017-09-02 19:07:14 -0300

    Adicionando Manager para Movies
    Manager é responsavel por manusear e armazenar os objetos.

---
+++ MyMovie/Manager/MMMovieManager.swift
@@ -0,0 +1,36 @@
+//
+//  MMMovieManager.swift
+//  MyMovie
+//
+//  Created by Gustavo Luís Soré on 02/09/17.
+//  Copyright © 2017 Gustavo Luís Soré. All rights reserved.
+//
+
+import UIKit
+
+class MMMovieManager: NSObject {
+
+    let business = MMMovieBusiness()
+    
+    var arrayMovies:[MMMovieModel] = []
+    
+    static let shared = MMMovieManager()
+    
+    func getPopularMovies(completion:@escaping (_ success:Bool)->Void){
+        
+        business.getPopularMovies { (success, arrayMovies) in
+            
+            if success == true{
+                
+                MMMovieManager.shared.arrayMovies = arrayMovies!
+                completion(true)
+                
+            } else{
+                completion(false)
+            }
+            
+        }
+        
+    }
+    
+}
